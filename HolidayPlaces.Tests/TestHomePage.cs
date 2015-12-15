62e2f80b9ec4fce09a8c31aa7755cd4e9565ce34
Nagarkar, Vikram (UK) <vikram.nagarkar@euromoneyplc.com>
2015-12-15 17:05:49 +0000

    Added Sample NUnit Tests

---
+++ HolidayPlaces.Tests/TestHomePage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace HolidayPlaces.Tests
+{
+    [TestFixture]
+    class TestHomepage
+    {
+        public TestHomepage()
+        {
+            
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [Test]
+        public void LoadHomePage()
+        {
+            
+        }
+
+        [Test]
+        public void UpdateHomePage()
+        {
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            
+        }
+    }
+}
